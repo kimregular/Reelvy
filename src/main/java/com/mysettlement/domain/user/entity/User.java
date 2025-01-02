f617c48f3df035fea8428c50db0b92738c5229dc
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-01-02 15:44:54 +0900

    #42-로그인-화면-제작 User 엔티티 수정

--- src/main/java/com/mysettlement/domain/user/entity/User.java
+++ src/main/java/com/mysettlement/domain/user/entity/User.java
@@ -5,6 +5,12 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -14,14 +20,18 @@ import static lombok.AccessLevel.PROTECTED;
 @Getter
 @Table(name = "USERS")
 @NoArgsConstructor(access = PROTECTED)
-public class User {
+public class User implements UserDetails {
 
     @Id
     @Column(name = "user_id")
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+
     private String password;
 
     @Column(name = "user_role")
@@ -35,6 +45,36 @@ public class User {
         this.email = email;
         this.password = password;
         this.userRole = userRole;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
     }
 
     public static User of(UserSigninRequestDto dto) {
