f31e97d42dfaf1bbe3777fa55500b3cfa1aa815b
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-01-02 15:57:48 +0900

    #42-로그인-화면-제작 UserService 수정

--- src/main/java/com/mysettlement/domain/user/controller/UserController.java
+++ src/main/java/com/mysettlement/domain/user/controller/UserController.java
@@ -4,7 +4,6 @@ import com.mysettlement.global.response.MySettlementGlobalResponse;
 import com.mysettlement.domain.user.entity.UserRole;
 import com.mysettlement.domain.user.dto.request.UserSigninRequestDto;
 import com.mysettlement.domain.user.dto.response.UserResponseDto;
-import com.mysettlement.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
--- src/main/java/com/mysettlement/domain/user/service/UserService.java
+++ src/main/java/com/mysettlement/domain/user/service/UserService.java
@@ -1,18 +1,55 @@
 package com.mysettlement.domain.user.service;
 
-import com.mysettlement.domain.user.entity.UserRole;
 import com.mysettlement.domain.user.dto.request.UserSigninRequestDto;
 import com.mysettlement.domain.user.dto.response.UserResponseDto;
+import com.mysettlement.domain.user.entity.User;
+import com.mysettlement.domain.user.entity.UserRole;
+import com.mysettlement.domain.user.exception.DuplicateUserException;
+import com.mysettlement.domain.user.exception.NoUserFoundException;
+import com.mysettlement.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
 @Transactional(readOnly = true)
-public interface UserService {
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
 
-    UserResponseDto findByUserName(String name);
+    private final UserRepository userRepository;
+
+
+    public UserResponseDto findByUserName(String name) {
+        User user = userRepository.findByName(name).orElseThrow(NoUserFoundException::new);
+        return UserResponseDto.of(user);
+    }
 
     @Transactional
-    UserResponseDto signinUser(UserSigninRequestDto userSigninRequestDto);
+    public UserResponseDto signinUser(UserSigninRequestDto userSigninRequestDto) {
+        if (isExistUser(userSigninRequestDto)) {
+            throw new DuplicateUserException();
+        }
+        User newUser = User.of(userSigninRequestDto);
+        userRepository.save(newUser);
+        return UserResponseDto.of(newUser);
+    }
 
     @Transactional
-    UserResponseDto changeUserStatus(Long userId, UserRole userRole);
+    public UserResponseDto changeUserStatus(Long userId, UserRole userRole) {
+        User foundUser = userRepository.findById(userId).orElseThrow(NoUserFoundException::new);
+        foundUser.update(userRole);
+        return UserResponseDto.of(foundUser);
+    }
+
+    private boolean isExistUser(UserSigninRequestDto userSigninRequestDto) {
+        return userRepository.findByEmail(userSigninRequestDto.getEmail()).isPresent();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                             .orElseThrow(NoUserFoundException::new);
+    }
 }
