6ce099b92c2fa39fcc18067bca77c3590ef139e9
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-01-25 23:23:22 +0900

    13-시큐리티-적용 modified : remove useless api

--- src/main/java/com/mysettlement/domain/user/service/UserService.java
+++ src/main/java/com/mysettlement/domain/user/service/UserService.java
@@ -1,16 +1,12 @@
 package com.mysettlement.domain.user.service;
 
 import com.mysettlement.domain.user.dto.request.EmailCheckRequestDto;
-import com.mysettlement.domain.user.dto.request.UserSignInRequestDto;
 import com.mysettlement.domain.user.dto.request.UserSignUpRequestDto;
 import com.mysettlement.domain.user.dto.response.EmailCheckResponseDto;
-import com.mysettlement.domain.user.dto.response.UserSignInResponseDto;
 import com.mysettlement.domain.user.dto.response.UserSignUpResponseDto;
 import com.mysettlement.domain.user.entity.User;
 import com.mysettlement.domain.user.exception.DuplicateUserException;
-import com.mysettlement.domain.user.exception.NoUserFoundException;
 import com.mysettlement.domain.user.repository.UserRepository;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -44,19 +40,6 @@ public class UserService {
 	public EmailCheckResponseDto checkEmail(EmailCheckRequestDto target) {
 		return new EmailCheckResponseDto(isExistUser(target.email()));
 	}
-
-	public UserSignInResponseDto signIn(@Valid UserSignInRequestDto userSignInRequestDto) {
-        User user = userRepository.findByEmail(userSignInRequestDto.email())
-                                  .orElseThrow(NoUserFoundException::new);
-        if(isInvalidPassword(user, userSignInRequestDto)) throw new NoUserFoundException();
-        return UserSignInResponseDto.of(user);
-	}
-
-    private boolean isInvalidPassword(User user, UserSignInRequestDto userSignInRequestDto) {
-	    String savedPassword = user.getPassword();
-	    String inputPassword = userSignInRequestDto.password();
-	    return !passwordEncoder.matches(inputPassword, savedPassword);
-    }
 
 	private boolean isExistUser(String email) {
 		return userRepository.existsByEmail(email);
