414ff0b4b43335131dca69fc06cb2cd0475ea256
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-01-27 01:31:14 +0900

    13-시큐리티-적용 로그인한 유저가 /login 접근 방지

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,7 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import SignupVue from "@/components/SignupView.vue";
 import HomeVue from "@/components/HomeView.vue";
 import LoginVue from "@/components/LoginView.vue";
+import {useAuthStore} from "@/stores/useAuthStore.ts";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,15 +10,27 @@ const router = createRouter({
     {
       path: '/',
       component: HomeVue,
+      name: "HOME"
     },
     {
       path: "/signup",
-      component: SignupVue
+      component: SignupVue,
+      name: "SIGNUP"
     },
     {
       path: "/login",
-      component: LoginVue
-    }
+      name: "LOGIN",
+      component: LoginVue,
+      beforeEnter: (to, from, next) => {
+        const authStore = useAuthStore();
+        if (authStore.token) {
+          // 로그인한 상태면 홈 페이지로 리다이렉트
+          next({path: "/"});
+        } else {
+          next(); // 로그인하지 않았으면 /login 접근 허용
+        }
+      },
+    },
   ],
 })
 
