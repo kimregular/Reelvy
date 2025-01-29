42309e90621468bfe000b058988fbec150d05957
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-01-30 06:31:42 +0900

    #49-비디오-업로드-조회-기능 코드 정리

--- src/main/java/com/mysettlement/global/configs/WebSecurityConfig.java
+++ src/main/java/com/mysettlement/global/configs/WebSecurityConfig.java
@@ -3,9 +3,7 @@ package com.mysettlement.global.configs;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mysettlement.global.jwt.filters.JwtAuthenticationFilter;
 import com.mysettlement.global.jwt.filters.JwtLoginFilter;
-import com.mysettlement.global.jwt.JwtProperties;
-import com.mysettlement.global.jwt.JwtProvider;
-import com.mysettlement.global.util.JwtUtils;
+import com.mysettlement.global.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,60 +20,56 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    private final JwtUtils jwtUtils;
-    private final JwtProvider jwtProvider;
-    private final ObjectMapper objectMapper;
-    private final JwtProperties jwtProperties;
-    private final AuthenticationConfiguration authenticationConfiguration;
+	private final JwtUtil jwtUtil;
+	private final ObjectMapper objectMapper;
+	private final AuthenticationConfiguration authenticationConfiguration;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/favicon.ico", "/error").permitAll()
-                        .requestMatchers(toH2Console()).permitAll()
-                        .requestMatchers(new AntPathRequestMatcher("/api/v1/user/login"),
-                                new AntPathRequestMatcher("/api/v1/user/signup"),
-                                new AntPathRequestMatcher("/api/v1/user/checkEmail"),
-                                new AntPathRequestMatcher("/")).permitAll()
-                        .anyRequest().authenticated())
-                .addFilterBefore(new JwtAuthenticationFilter(jwtProperties, jwtUtils), JwtLoginFilter.class)
-                .addFilterAt(jwtLoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class).build();
-    }
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.headers(header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+				.csrf(AbstractHttpConfigurer::disable)
+				.formLogin(AbstractHttpConfigurer::disable)
+				.httpBasic(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/favicon.ico", "/error").permitAll()
+						.requestMatchers("/h2-console/**").permitAll()
+						.requestMatchers(new AntPathRequestMatcher("/api/v1/user/login"),
+								new AntPathRequestMatcher("/api/v1/user/signup"),
+								new AntPathRequestMatcher("/api/v1/user/checkEmail"),
+								new AntPathRequestMatcher("/")).permitAll()
+						.anyRequest().authenticated())
+				.addFilterBefore(jwtAuthenticationFilter(), JwtLoginFilter.class)
+				.addFilterAt(jwtLoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class).build();
+	}
 
-    @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter(jwtProperties, jwtUtils);
-    }
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter(jwtUtil);
+	}
 
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
+	}
 
 
-    @Bean
-    public JwtLoginFilter jwtLoginFilter(AuthenticationManager authenticationManager) {
-        JwtLoginFilter jwtLoginFilter = new JwtLoginFilter(authenticationManager, jwtProperties, objectMapper, jwtProvider);
-        jwtLoginFilter.setAuthenticationManager(authenticationManager);
-        jwtLoginFilter.setFilterProcessesUrl("/api/v1/user/login"); // 로그인 URL 설정
-        return jwtLoginFilter;
-    }
+	@Bean
+	public JwtLoginFilter jwtLoginFilter(AuthenticationManager authenticationManager) {
+		JwtLoginFilter jwtLoginFilter = new JwtLoginFilter(authenticationManager, jwtUtil, objectMapper);
+		jwtLoginFilter.setAuthenticationManager(authenticationManager);
+		jwtLoginFilter.setFilterProcessesUrl("/api/v1/user/login"); // 로그인 URL 설정
+		return jwtLoginFilter;
+	}
 
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 }
