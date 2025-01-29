66dd1d7c4c784d8af1e81e23ae7e1510b24783bf
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-01-30 06:05:24 +0900

    #49-비디오-업로드-조회-기능 JwtProperties.java 리팩터링

--- src/main/java/com/mysettlement/global/jwt/JwtProperties.java
+++ src/main/java/com/mysettlement/global/jwt/JwtProperties.java
@@ -1,15 +1,8 @@
 package com.mysettlement.global.jwt;
 
-import io.jsonwebtoken.security.Keys;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-
-import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 
 @ConfigurationProperties("jwt")
 public record JwtProperties(String HEADER, String BEARER, String ISSUER, Long TOKEN_LIFETIME, String SECRET_KEY) {
 
-	public SecretKey getSigningKey() {
-		return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
-	}
 }
