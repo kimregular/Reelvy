ba9535550d60caeaff692554d772767da1b9870b
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-02-25 22:40:59 +0900

    #49-비디오-업로드-조회-기능
    비디오 응답에 조회수 추가

--- src/main/java/com/mysettlement/domain/video/dto/response/VideoResponseDto.java
+++ src/main/java/com/mysettlement/domain/video/dto/response/VideoResponseDto.java
@@ -11,18 +11,21 @@ public class VideoResponseDto {
     private final UserResponseDto user;
     private final String desc;
     private final String videoPath;
+    private final long videoView;
 
-    private VideoResponseDto(String title, UserResponseDto user, String desc, String videoPath) {
+    private VideoResponseDto(String title, UserResponseDto user, String desc, String videoPath, long videoView) {
         this.title = title;
         this.user = user;
         this.desc = desc;
         this.videoPath = videoPath;
+	    this.videoView = videoView;
     }
 
     public static VideoResponseDto of(Video video) {
         return new VideoResponseDto(video.getVideoTitle(),
                 UserResponseDto.of(video.getUser()),
                 video.getVideoDesc(),
-                video.getVideoPath());
+                video.getVideoPath(),
+                video.getVideoView());
     }
 }
