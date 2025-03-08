8d024ba1d008a7d32245c1a4cd1549e7eacfdf6f
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-03-09 02:51:44 +0900

    #49-비디오-업로드-조회-기능
    비디오 재생 적용

--- front/src/components/HomeView.vue
+++ front/src/components/HomeView.vue
@@ -3,28 +3,34 @@ import {onMounted, ref} from "vue";
 import axios from "axios";
 import {BASE_URL} from "@/constants/server.ts";
 import type Video from "@/types/video.ts";
+import router from "@/router";
 
 const videos = ref<Video[]>([]);
 const loading = ref(true);
 
 const requestVideos = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}/v1/video/videos`);
-    const videosDatas : Video[] = response.data;
+    const response = await axios.get(`${BASE_URL}/v1/videos`);
+    const videosData: Video[] = response.data;
     loading.value = false;
-    videos.value = videosDatas;
+    videos.value = videosData;
   } catch (error) {
     console.log("비디오 로딩 실패!")
   }
 }
 onMounted(requestVideos);
+
+const watch = (videoId: number) => {
+  router.push({name: "WATCH", query: {videoId: videoId.toString()}});
+}
+
 </script>
 
 <template>
   <div style="padding: 15px">
     <div v-if="loading">loading...</div>
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-4" v-else>
-      <div class="col video-link" v-for="(video, idx) in videos" :key="idx">
+    <div v-else class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-4">
+      <div class="col video-link" v-for="video in videos" :key="video.id" @click="watch(video.id)">
         <div class="card">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvX7ghSY75PvK5S-RvhkFxNz88MWEALSBDvA&s"
                class="card-img-top" alt="...">
