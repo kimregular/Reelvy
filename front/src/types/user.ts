5582be4631aea2fe6c1a25c10b7a7419e55b3e51
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-03-18 00:29:24 +0900

    #64-유저-이름과-이메일-구분-필요
    유저 필드 변경 반영

--- front/src/types/user.ts
+++ front/src/types/user.ts
@@ -1,4 +1,7 @@
 export default interface User {
-  name : string;
-  email : string;
+  username: string;
+  nickname: string;
+  desc: string,
+  profileImageUrl: string,
+  backgroundImageUrl: string;
 }
