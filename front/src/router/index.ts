3dcb629fdc99ac441fc583a317bd708562f51114
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-03-18 00:30:01 +0900

    #64-유저-이름과-이메일-구분-필요
    notfound 페이지 생성, 반영

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -9,6 +9,8 @@ import LoginView from "@/components/LoginView.vue";
 import WatchView from "@/components/WatchView.vue";
 import UploadView from "@/components/UploadView.vue";
 import {requireAnonymous, requireAuth} from "@/utils/routerUtil.ts";
+import ProfileView from "@/components/ProfileView.vue";
+import NotFoundView from "@/components/errorPages/NotFoundView.vue";
 
 
 const router = createRouter({
@@ -38,10 +40,24 @@ const router = createRouter({
     },
     {
       path: "/login",
-      name: "LOGIN",
       component: LoginView,
+      name: "LOGIN",
       beforeEnter: requireAnonymous
     },
+    {
+      path: "/profile/:username",
+      component: ProfileView,
+      name: "PROFILE",
+    },
+    {
+      path: "/notFound",
+      component: NotFoundView,
+      name: "NOT_FOUND"
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: {name: "NOT_FOUND"}
+    }
   ],
 });
 
