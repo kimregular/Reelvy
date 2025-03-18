72aa2d6400f66af3ff6adb3d479b19948b4bc87d
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-03-18 11:32:10 +0900

    #58-프로필-프론트-작업
    프론트에서 사용할 클래스 정의

--- front/src/types/user.ts
+++ front/src/types/user.ts
@@ -1,7 +1,44 @@
-export default interface User {
-  username: string;
-  nickname: string;
-  desc: string,
-  profileImageUrl: string,
-  backgroundImageUrl: string;
+export default class User {
+  private readonly _username: string;
+  private readonly _nickname: string;
+  private readonly _desc: string;
+  private readonly _profileImageUrl: string;
+  private readonly _backgroundImageUrl: string;
+
+
+  constructor(username: string, nickname: string, desc: string, profileImageUrl: string, backgroundImageUrl: string) {
+    this._username = username;
+    this._nickname = nickname;
+    this._desc = desc;
+    this._profileImageUrl = profileImageUrl;
+    this._backgroundImageUrl = backgroundImageUrl;
+  }
+
+  public static of(data:any): User {
+    return new User(data.username,
+      data.nickname,
+      data.desc,
+      data._profileImageUrl,
+      data._backgroundImageUrl);
+  }
+
+  get username(): string {
+    return this._username;
+  }
+
+  get nickname(): string {
+    return this._nickname;
+  }
+
+  get desc(): string {
+    return this._desc;
+  }
+
+  get profileImageUrl(): string {
+    return this._profileImageUrl;
+  }
+
+  get backgroundImageUrl(): string {
+    return this._backgroundImageUrl;
+  }
 }
--- front/src/types/video.ts
+++ front/src/types/video.ts
@@ -1,10 +1,55 @@
 import type User from "@/types/user.ts";
+import router from "@/router";
 
-export default interface Video {
-  id : number
-  title : string;
-  user : User;
-  desc : string;
-  videoPath : string;
-  videoView : number;
+export default class Video {
+  private readonly _id : number
+  private readonly _title : string;
+  private readonly _user : User;
+  private readonly _desc : string;
+  private readonly _videoView : number;
+
+  constructor(id: number, title: string, user: User, desc: string, videoView: number) {
+    this._id = id;
+    this._title = title;
+    this._user = user;
+    this._desc = desc;
+    this._videoView = videoView;
+    this.watch = this.watch.bind(this)
+  }
+
+  public static of(data: any, user : User): Video {
+    return new Video(
+      data.id,
+      data.title,
+      user,
+      data.desc,
+      data.videoView
+    );
+  }
+
+
+  get id(): number {
+    return this._id;
+  }
+
+  get title(): string {
+    return this._title;
+  }
+
+  get user(): User {
+    return this._user;
+  }
+
+  get desc(): string {
+    return this._desc;
+  }
+
+  get videoView(): number {
+    return this._videoView;
+  }
+
+  public async watch() {
+    console.log("video watch func has been called!");
+    await router.push({name: "WATCH", query: {videoId: this.id}});
+  }
 }
