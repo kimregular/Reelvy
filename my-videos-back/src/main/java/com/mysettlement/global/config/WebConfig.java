2bd6cdc102bdae674dcf4dbb043fd7be51129e8e
Regular Kim <82751941+kimregular@users.noreply.github.com>
2025-06-07 07:09:18 +0900

    #loginUser-check
    @LoginUser 에너테이션 제작, 리졸버 등록

--- my-videos-back/src/main/java/com/mysettlement/global/config/WebConfig.java
+++ my-videos-back/src/main/java/com/mysettlement/global/config/WebConfig.java
@@ -1,12 +1,20 @@
 package com.mysettlement.global.config;
 
+import com.mysettlement.global.resolver.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+	private final LoginUserArgumentResolver loginUserArgumentResolver;
 
 	@Value("${app.upload-dir}")
 	private String uploadDir;
@@ -15,5 +23,10 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/app/uploads/**") // 응답의 "app/uploads/..."와 매핑
 				.addResourceLocations("file:" + uploadDir + "/"); // 실제 저장 경로: app/uploads/
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(loginUserArgumentResolver);
 	}
 }
